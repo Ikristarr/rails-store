14c2a5141d63a5b9eb9b62045bd49a25f08046cf
ikristar <iykejchurchill@gmail.com>
2026-09-17 10:07:18 +0100

    Refactor product form into partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,16 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update ]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
-
 
   def new
     @product = Product.new
-  end 
-
+  end
 
   def create
     @product = Product.new(product_params)
@@ -23,7 +22,22 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.expect(product: [ :name ])
